aa89f0756ab58abaad74e7ab1a98476d26c4c905
phlemy <phlemy@gmail.com>
2020-05-11 22:18:03 +1000

    3

--- get-quote.py
+++ get-quote.py
@@ -7,8 +7,14 @@ def primary():
   f.close()
 
   last = 13
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0,last)
+  rnd2 = random.randint(0,last)
+  while rnd2 == rnd1:
+    rnd2 = random.randint(0,last)
+
+  print(quotes[rnd1], end = "@@")
+  print(quotes[rnd2], end = "^^")
+  
 
 if __name__== "__main__":
   primary()
