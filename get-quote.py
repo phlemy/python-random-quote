843b9cc93001f8e3708890e848529f7dd55aeae4
phlemy <phlemy@gmail.com>
2020-05-12 12:10:30 +1000

    hello

--- get-quote.py
+++ get-quote.py
@@ -1,20 +1,19 @@
 import random
 def primary():
-  # print("Keep it logically awesome.")
-
+  
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.readlines() # quotes is an array of lines
   f.close()
 
-  last = 13
+  last = len(quotes) - 1
   rnd1 = random.randint(0,last)
   rnd2 = random.randint(0,last)
   while rnd2 == rnd1:
-    rnd2 = random.randint(0,last)
+   rnd2 = random.randint(0,last)
+  rndQuote1=quotes[rnd1]
+  rndQuote2=quotes[rnd2]
 
-  print(quotes[rnd1], end = "@@")
-  print(quotes[rnd2], end = "^^")
+  print(rndQuote1[0:len(rndQuote1)-1], rndQuote2[0:len(rndQuote2)-1], end=" -- ")
   
-
 if __name__== "__main__":
   primary()
